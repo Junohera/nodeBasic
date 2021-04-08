11e9c9676e80d19a4d4d1fc46e88b9f90f61a751
juno <selectjuno@gmail.com>
2021-04-08 12:00:53 +0900

    소스 영역 분리 및 설명 추가

--- 04_Express_Server/npm03/app.js
+++ 04_Express_Server/npm03/app.js
@@ -5,19 +5,33 @@ const morgan = require('morgan'); // 각각의 요청과 응답에 대한 필요
 const cookieParser = require('cookie-parser'); // 쿠키 사용을 간결히 사용하기위함
 const session = require('express-session'); // 세션 사용을 간결히 사용하기위함
 
-const app = express();
+// ! <config>
+    const app = express();
+    app.set('port', process.env.PORT || 3000);
+// ! </config>
 
-app.set('port', process.env.PORT || 3000);
+// ! <common-middleware-config>
+    /** 
+     * ! morgan
+     * * 실행결과 GET / 200 5.316 ms - 165
+     * * method 방식, 응답 결과코드, 요청과 실행에 걸린 시간 등등
+     * ? app.use(morgan('combined')); 으로 설정시 더 자세한 내용을 볼 수도 있음.
+     */
+    app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ::: ${req.url}`);
-    next();
-});
+    app.use(cookieParser());
+    
+// ! </common-middleware-config>
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+// ! <playground>
+    app.get('/', (req, res) => {
+        // res.sendFile(path.join(__dirname, 'index.html'));
+        res.send('<h1>Hello Express Server~!!</h1>');
+    });
+// ! </playground>
 
+// ! <config>
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), 'is running ... ');
 });
+// ! </config>
