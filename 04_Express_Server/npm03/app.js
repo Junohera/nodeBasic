983b86c798fba422996916dd6a28f0fcf4ff684d
juno <selectjuno@gmail.com>
2021-04-08 13:37:35 +0900

    setting

--- 04_Express_Server/npm03/app.js
+++ 04_Express_Server/npm03/app.js
@@ -17,16 +17,45 @@ const session = require('express-session'); // 세션 사용을 간결히 사용
      * * method 방식, 응답 결과코드, 요청과 실행에 걸린 시간 등등
      * ? app.use(morgan('combined')); 으로 설정시 더 자세한 내용을 볼 수도 있음.
      */
-    app.use(morgan('dev'));
+    app.use(morgan('combined'));
 
     app.use(cookieParser());
+
+    app.use(express.json()); // bodyParser json : formData 사용을 위한 모듈
+    app.use(express.urlencoded({extended:true}));  // bodyParser formData modules
+    app.use(session({
+        resave: false,
+        saveUninitialized: false,
+        secret: 'junojunojuno',
+    })); // 세션 활용을 위한 미들웨어
     
 // ! </common-middleware-config>
 
 // ! <playground>
+
     app.get('/', (req, res) => {
-        // res.sendFile(path.join(__dirname, 'index.html'));
-        res.send('<h1>Hello Express Server~!!</h1>');
+        // 1. 저장된 쿠키를 불러와 활용할 변수 res.cookies 확인
+        console.log(req.cookies);
+
+        // 2. 새로운 쿠키의 저장
+        const name = '홍길동';
+        res.cookie('name', encodeURIComponent(name),{
+            expires: new Date(),
+            httpOnly: true,
+            path: '/'
+        });
+
+        // 3. 쿠키의 삭제
+        /*
+        res.clearCookie('name', encodeURIComponent(name), {
+            httpOnly: true,
+            path: '/',
+        });
+        */
+
+
+        res.sendFile(path.join(__dirname, 'index.html'));
+        // res.send('<h1>Hello Express Server~!!</h1>');
     });
 // ! </playground>
 
