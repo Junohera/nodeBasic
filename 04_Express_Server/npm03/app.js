f2b860e99c38c3a2f68d79cb06a78e8667d09975
juno <selectjuno@gmail.com>
2021-04-08 11:49:59 +0900

    새로운 모듈 추가 morgan, cookieParser, session

--- 04_Express_Server/npm03/app.js
+++ 04_Express_Server/npm03/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 
