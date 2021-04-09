3c4da2302a3bece3331e13be53599c9aff3f9860
juno <selectjuno@gmail.com>
2021-04-09 11:57:49 +0900

    주석 삭제

--- 04_Express_Server/06_npm/app.js
+++ 04_Express_Server/06_npm/app.js
@@ -1,31 +1,24 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-// ! <config>
-    const app = express();
-    app.set('port', process.env.PORT || 3000);
-// ! </config>
-// ! <common-middleware-config>
-    app.use(morgan('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({extended:true}));
-// ! </common-middleware-config>
-// ! <playground>
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
-    app.get('*', (req, res) => {
-        throw new Error('no page');
-    });
-// ! </playground>
-// ! <Advice>
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('*', (req, res) => {
+    throw new Error('no page');
+});
+
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(404).send(err.message);
 });
-// ! </Advice>
-// ! <config>
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), 'is running ... ');
 });
-// ! </config>
