f79cd6b6e74de13dc3e302277bf7bd1d9fe70165
juno <selectjuno@gmail.com>
2021-04-12 09:47:36 +0900

    merge

---
+++ 04_Express_Server/08_npm/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const User = require('../models/user');
+const Comment = require('../models/comment');
+
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    
+});
+
+module.exports = router;
