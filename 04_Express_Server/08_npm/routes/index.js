7efdfb0037aad0da5a5a962d5793dd6978bc6167
juno <selectjuno@gmail.com>
2021-04-12 10:01:20 +0900

    ?

--- 04_Express_Server/08_npm/routes/index.js
+++ 04_Express_Server/08_npm/routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const User = require('../models/user');
 const Comment = require('../models/comment');
+const { noExtendLeft } = require('sequelize/types/lib/operators');
 
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    
+    try {
+        res.render('sequelize', {});
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }    
 });
 
 module.exports = router;
