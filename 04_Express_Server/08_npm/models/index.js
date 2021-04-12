34950d219921a486a87a200f71bb79f5bebdcab5
juno <selectjuno@gmail.com>
2021-04-12 11:21:25 +0900

    dbinfo 삭제

--- 04_Express_Server/08_npm/models/index.js
+++ 04_Express_Server/08_npm/models/index.js
@@ -6,7 +6,7 @@ const Comment = require('./comment');
 
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/dbinfo.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
